8e6f20304a92ecda6c6fc8ae6140c5dc6b05e95b
freerole <freerole@218.38.137.28>
2021-05-17 23:52:13 +0900

    불필요한 출력 삭제

--- src/calendar/calendar.java
+++ src/calendar/calendar.java
@@ -9,8 +9,7 @@ public class calendar {
 		return Max_Days[month - 1];
 	}
 	
-	public static void main(String[] args) {
-		System.out.println("Hello.Calendar");	
+	public void calendarSample() {
 		System.out.println(" 일  월   화  수  목   금   토");
 		System.out.println("--------------------------");
 		System.out.println(" 1   2   3   4   5   6   7");
@@ -18,6 +17,10 @@ public class calendar {
 		System.out.println("15  16  17  18  19  20  21");
 		System.out.println("22  23  24  25  26  27  28");
 		System.out.println("29  30  31                ");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello.Calendar");	
 		
 		System.out.println("알고싶으신 달 입력 하세요.");		
 		Scanner scanner = new Scanner(System.in);
@@ -25,7 +28,7 @@ public class calendar {
 		
 		calendar cal = new calendar();
 		int day = cal.maxDayOfMonth(month);
-		
+		//cal.calendarSample();
 		System.out.printf("%d 달의 마지막 날은 %d 입니다.", month, day);
 		
 	}	
